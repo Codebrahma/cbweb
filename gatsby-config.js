435d6ed86802092e3ee381c2724b20c8d63db784
Anand Narayan <i@codeanand.com>
2019-06-20 09:08:19 -0700

    moving content outside of src

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,14 +59,14 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `posts`,
-        path: `${__dirname}/src/posts/`
+        path: `${__dirname}/posts/`
       }
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `services`,
-        path: `${__dirname}/src/services/`
+        path: `${__dirname}/services/`
       }
     },
 
