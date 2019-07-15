1c09033fd43b9235fbcfd220cf382781bd1a543f
Kameshwaran Sachithanantham <kamesh@codebrahma.com>
2019-07-16 00:15:19 +0530

    Meta referrer

--- src/templates/layout.js
+++ src/templates/layout.js
@@ -11,66 +11,90 @@ import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 import "./fonts.css"
 
-import { ThemeProvider, Flex, Container,
-   Box, P, InputButton, InputText, HorizontalRule, B } from 'bricks'
-import { Global, css } from '@emotion/core'
+import {
+  ThemeProvider,
+  Flex,
+  Container,
+  Box,
+  P,
+  InputButton,
+  InputText,
+  HorizontalRule,
+  B,
+} from "bricks"
+import { Global, css } from "@emotion/core"
 import Header from "./header"
-import theme from '../theme'
+import theme from "../theme"
 // TODO MDXProvider need not be used as theme ui set the components automatically
 // due to ambiguity on what gatsby theme ui does, we are yet to implement this
-import { MDXProvider } from "@mdx-js/react";
-import { useThemeUI } from 'bricks';
-import  PlainLink  from '../components/link'
-import {Helmet} from "react-helmet";
+import { MDXProvider } from "@mdx-js/react"
+import { useThemeUI } from "bricks"
+import PlainLink from "../components/link"
+import { Helmet } from "react-helmet"
 
 const Footer = () => (
-  <Box marginTop='6' pb='6'>
-  <HorizontalRule
-    width={1}
-  />
-  <Flex flexDirection={['column','row']} justifyContent='space-between' mt='3'>
-    <Box width={[1, 1/3]}>
-      <P>
-      Join our <B>NEW</B> newsletter 
-      to learn about the latest trends 
-      in the fast changing front end atmosphere
-      </P>
-      <Box my='1'>
-        <form 
-          action="https://codebrahma.us3.list-manage.com/subscribe/post" 
-          method="post" target='/thanks'>
-          <input type="hidden" name="u" value="42069ce8928af0d4afc3fd428"/>
-          <input type="hidden" name="id" value="a540ccf305"/>
-          <InputText name='MERGE0' type='email' placeholder='Email address'/>
-          <InputButton value='Subscribe' name='subscribe'/>
-        </form>
-      </Box>
-    </Box>
-    <Box width={[1,1/4]} mt={[2,0]}>
-      <P>
-        <PlainLink as='a' to="mailto:hello@codebrahma.com">hello@codebrahma.com</PlainLink><br/>
-        +1 484 506 0634<br/>
-      </P>
-      <Box mt='1'>
+  <Box marginTop="6" pb="6">
+    <HorizontalRule width={1} />
+    <Flex
+      flexDirection={["column", "row"]}
+      justifyContent="space-between"
+      mt="3"
+    >
+      <Box width={[1, 1 / 3]}>
         <P>
-            <PlainLink as='a' to="https://twitter.com/codebrahma">Twitter</PlainLink><br/>
-            <PlainLink as='a' to="https://github.com/codebrahma">Github</PlainLink><br/>
+          Join our <B>NEW</B> newsletter to learn about the latest trends in the
+          fast changing front end atmosphere
+        </P>
+        <Box my="1">
+          <form
+            action="https://codebrahma.us3.list-manage.com/subscribe/post"
+            method="post"
+            target="/thanks"
+          >
+            <input type="hidden" name="u" value="42069ce8928af0d4afc3fd428" />
+            <input type="hidden" name="id" value="a540ccf305" />
+            <InputText name="MERGE0" type="email" placeholder="Email address" />
+            <InputButton value="Subscribe" name="subscribe" />
+          </form>
+        </Box>
+      </Box>
+      <Box width={[1, 1 / 4]} mt={[2, 0]}>
+        <P>
+          <PlainLink as="a" to="mailto:hello@codebrahma.com">
+            hello@codebrahma.com
+          </PlainLink>
+          <br />
+          +1 484 506 0634
+          <br />
+        </P>
+        <Box mt="1">
+          <P>
+            <PlainLink as="a" to="https://twitter.com/codebrahma">
+              Twitter
+            </PlainLink>
+            <br />
+            <PlainLink as="a" to="https://github.com/codebrahma">
+              Github
+            </PlainLink>
+            <br />
+          </P>
+        </Box>
+      </Box>
+      <Box width={[1, 1 / 4]} mt={[2, 0]}>
+        <P>
+          156, 2nd Street
+          <br />
+          San Francisco, CA 94105
+          <br />
         </P>
       </Box>
-    </Box>
-    <Box width={[1,1/4]} mt={[2,0]}>
-      <P>
-        156, 2nd Street<br/>
-        San Francisco, CA 94105<br/>
-      </P>
-    </Box>
-  </Flex>
+    </Flex>
   </Box>
 )
 
 const Layout = ({ children }) => {
-  const context = useThemeUI();
-  const comps   = context.components;
+  const context = useThemeUI()
+  const comps = context.components
   return (
     <StaticQuery
       query={graphql`
@@ -85,27 +109,26 @@ const Layout = ({ children }) => {
       render={data => (
         <ThemeProvider theme={theme}>
           <MDXProvider components={comps}>
-            <div style={{background: 'rgb(247,245,242)', minHeight: '100vh',}}>
+            <div style={{ background: "rgb(247,245,242)", minHeight: "100vh" }}>
               <Global
                 styles={css`
-                        a:visited { 
-                          color: inherit;
-                        }
-                        a:hover {
-                          cursor: grab;  
-                        }
-                        body {
-                          font-family: ${theme.fonts.body};
-                          color: ${theme.colors.black[1]};
-                        }
-                  `}/>
-              <Helmet>
-                <meta name="referrer" content="always"/>
-              </Helmet>
+                  a:visited {
+                    color: inherit;
+                  }
+                  a:hover {
+                    cursor: grab;
+                  }
+                  body {
+                    font-family: ${theme.fonts.body};
+                    color: ${theme.colors.black[1]};
+                  }
+                `}
+              />
+              <Helmet meta={[{ name: "referrer", content: "origin" }]} />
               <Container>
                 <Header siteTitle={data.site.siteMetadata.title} />
                 <div>
-                  <main>{ children }</main>
+                  <main>{children}</main>
                   <footer>
                     <Footer></Footer>
                   </footer>
