41d714fdd973fb65372c34f2a159d5c588dd3eb1
Kameshwaran Sachithanantham <kamesh@codebrahma.com>
2019-07-16 23:54:05 +0530

    Loaded script on document.ready (#14)
    * Loaded script on document.ready
    
    * Added comment on chart-loader.js

--- static/scripts/chart-loader.js
+++ static/scripts/chart-loader.js
@@ -1,16 +1,21 @@
-var chartContainer = document.getElementById("google-chart")
-trends.embed.renderExploreWidgetTo(
-  chartContainer,
-  "TIMESERIES",
-  {
-    comparisonItem: [
-      { keyword: "blockchain", geo: "", time: "2018-07-11 2019-07-11" },
-    ],
-    category: 0,
-    property: "",
-  },
-  {
-    exploreQuery: "q=blockchain&date=today 12-m",
-    guestPath: "https://trends.google.com:443/trends/embed/",
+// Script to load the block chain chart on DOM element with the id 'google-chart'
+document.onreadystatechange = function() {
+  if (document.readyState === 'complete') {
+    var chartContainer = document.getElementById("google-chart")
+    trends.embed.renderExploreWidgetTo(
+      chartContainer,
+      "TIMESERIES",
+      {
+        comparisonItem: [
+          { keyword: "blockchain", geo: "", time: "2018-07-11 2019-07-11" },
+        ],
+        category: 0,
+        property: "",
+      },
+      {
+        exploreQuery: "q=blockchain&date=today 12-m",
+        guestPath: "https://trends.google.com:443/trends/embed/",
+      }
+    )
   }
-);
+};
