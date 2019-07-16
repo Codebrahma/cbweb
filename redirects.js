29c45a30b623daa6d80a9157100ddcced9d4046a
Kameshwaran <kamesh@codebrahma.com>
2019-07-16 11:41:17 +0530

    Moved redirects to different file and added redirects for blog and pricing

---
+++ redirects.js
@@ -0,0 +1,19 @@
+module.exports = [
+  {
+    from: "/deploy-react-application-depth-overview-various-option",
+    to: "/deploy-react-application-depth-overview-various-options-deploy",
+  },
+  {
+    from: "/reselect-tutorial-optimizing-react-redux-application-d",
+    to:
+      "/reselect-tutorial-optimizing-react-redux-application-development-with-reselect",
+  },
+  {
+    from: "/blogs",
+    to: "/blog",
+  },
+  {
+    from: "/pricing",
+    to: "/faq",
+  },
+]
