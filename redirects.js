347447799548a46c42357dfc3afff46e1bf8bc2b
Kameshwaran Sachithanantham <kamesh@codebrahma.com>
2019-07-18 10:30:19 +0530

    Addition of redirects and robots txt (#23)
    * Added robots.txt
    
    * Added redirects for react, react native, ruby and rails service alias pages
    
    * Added unavailable url redirects
    
    * Added command to copy robots.txt to public directory after building
    
    * Removed the copy command to copy robots.txt and moved the robots.txt to static folder

--- redirects.js
+++ redirects.js
@@ -20,4 +20,239 @@ module.exports = [
     from: "/services",
     to: "/solutions",
   },
+  {
+    from: "/develop-react-applications",
+    to: "/react-js-development",
+  },
+  {
+    from: "/ruby-consultants",
+    to: "/ruby-on-rails-developers-consulting-bangalore",
+  },
+  {
+    from: "/rails_consultants",
+    to: "/ruby-on-rails-developers-consulting-bangalore",
+  },
+  {
+    from: "/ruby_developers",
+    to: "/ruby-on-rails-developers-consulting-bangalore",
+  },
+  {
+    from: "/ruby_rails_developers",
+    to: "/ruby-on-rails-developers-consulting-bangalore",
+  },
+  {
+    from: "/mobile-application-development-company-bangalore",
+    to: "/react-native-development-company",
+  },
+
+
+  /**
+   * TEMPORARY REDIRECTS TO AVOID GOOGLE'S PENALTY
+   *
+   * We have submitted the below URLs in google webmaster to remove it from its index, because these * urls are no longer available in codebrahma.com.
+   *
+   * We are adding temporary redirects to prevent users encoutering 404 for below URLs, till it gets * removed from google's index.
+   *
+   * This code should be removed once Google removed the URLs from google's index.
+   *
+   * */
+  {
+    from: "/code-reviews/",
+    to: "/",
+  },
+  {
+    from: "/design-cheapercredit/",
+    to: "/",
+  },
+  {
+    from: "/cb-recruitment-2016/",
+    to: "/",
+  },
+  {
+    from: "/confirm-subscription/",
+    to: "/",
+  },
+  {
+    from: "/job/rails-engineer/",
+    to: "/",
+  },
+  {
+    from: "/job/full-stack-developer/",
+    to: "/",
+  },
+  {
+    from: "/quickburp/",
+    to: "/",
+  },
+  {
+    from: "/zenkars/",
+    to: "/",
+  },
+  {
+    from: "/tutorscoop/",
+    to: "/",
+  },
+  {
+    from: "/scheduling-jquery-plugin-2/",
+    to: "/",
+  },
+  {
+    from: "/prism/",
+    to: "/",
+  },
+  {
+    from: "/letsventure/",
+    to: "/",
+  },
+  {
+    from: "/lorious/",
+    to: "/",
+  },
+  {
+    from: "/filmzu/",
+    to: "/",
+  },
+  {
+    from: "/castasy/",
+    to: "/",
+  },
+  {
+    from: "/group-rocket-meteor-development/",
+    to: "/",
+  },
+  {
+    from: "/juspay/",
+    to: "/",
+  },
+  {
+    from: "/yuemey/",
+    to: "/",
+  },
+  {
+    from: "/hipstart/",
+    to: "/",
+  },
+  {
+    from: "/whalelogix/",
+    to: "/",
+  },
+  {
+    from: "/know-how-shopper/",
+    to: "/",
+  },
+  {
+    from: "/top-advisors/",
+    to: "/",
+  },
+  {
+    from: "/seechat/",
+    to: "/",
+  },
+  {
+    from: "/meteor-js-development-company/",
+    to: "/",
+  },
+  {
+    from: "/salesforce-consulting-services/",
+    to: "/",
+  },
+  {
+    from: "/saas-development-services/",
+    to: "/",
+  },
+  {
+    from: "/mandrill/",
+    to: "/",
+  },
+  {
+    from: "/scipy/",
+    to: "/",
+  },
+  {
+    from: "/numpy/",
+    to: "/",
+  },
+  {
+    from: "/mobile-chat-solution/",
+    to: "/",
+  },
+  {
+    from: "/ipad-app-development-company/",
+    to: "/",
+  },
+  {
+    from: "/custom-software-development-company/",
+    to: "/",
+  },
+  {
+    from: "/chatbot-development-company-bangalore/",
+    to: "/",
+  },
+  {
+    from: "/outsourced-software-product-development/",
+    to: "/",
+  },
+  {
+    from: "/android-development-company-in-bangalore/",
+    to: "/",
+  },
+  {
+    from: "/hire-angularjs-development-company/",
+    to: "/",
+  },
+  {
+    from: "/ecommerce-development-compan/",
+    to: "/",
+  },
+  {
+    from: "/spree-commerce-development/",
+    to: "/",
+  },
+  {
+    from: "/uiux-development-company/",
+    to: "/",
+  },
+  {
+    from: "/e-learning-applications/",
+    to: "/",
+  },
+  {
+    from: "/graphql-development-company/",
+    to: "/",
+  },
+  {
+    from: "/front-end-development-company/",
+    to: "/",
+  },
+  {
+    from: "/serviceblockchain-development-company/",
+    to: "/",
+  },
+  {
+    from: "/iphone-ios-app-development-company-in-bangalore/",
+    to: "/",
+  },
+  {
+    from: "/service-vertical/force-platform/",
+    to: "/",
+  },
+  {
+    from: "/service-vertical/integration/",
+    to: "/",
+  },
+  {
+    from: "/service-vertical/machine-learning/",
+    to: "/",
+  },
+  {
+    from: "/service-vertical/mobile/",
+    to: "/",
+  },
+  {
+    from: "/service-vertical/web/",
+    to: "/",
+  },
+  /**
+   * TEMPORARY REDIRECTS ENDS HERE
+   */
 ]
