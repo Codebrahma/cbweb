13baf5c43a353b0dd37249d0893cad14dbb92cd0
sachinmjadhav <sachin0321@gmail.com>
2019-07-23 10:48:21 +0530

    indentation fix

--- src/templates/header.js
+++ src/templates/header.js
@@ -13,86 +13,86 @@ const links = [
 
 class Header extends Component {
 
-	componentDidMount() {
-		if(typeof window !== 'undefined') {
-			let userDetails = {}
-			let SITE_NAME = window.location.origin;
-			document.getElementById('contact-form') &&
-			document
-				.getElementById('contact-form')
-				.addEventListener('submit', (e => {
-					e.preventDefault()
-					const idea = document.getElementById('idea').value
-					const email = document.getElementById('email').value
-					document.getElementById('idea').value = ""
-					document.getElementById('email').value = ""
-	
-					const formSubmitData = {
-						email,
-						idea,
-						userDetails,
-					}
-	
-					const url = 'https://hooks.zapier.com/hooks/catch/1041785/21ewe5/'
+  componentDidMount() {
+    if(typeof window !== 'undefined') {
+      let userDetails = {}
+      let SITE_NAME = window.location.origin;
+      document.getElementById('contact-form') &&
+      document
+        .getElementById('contact-form')
+        .addEventListener('submit', (e => {
+          e.preventDefault()
+          const idea = document.getElementById('idea').value
+          const email = document.getElementById('email').value
+          document.getElementById('idea').value = ""
+          document.getElementById('email').value = ""
+  
+          const formSubmitData = {
+            email,
+            idea,
+            userDetails,
+          }
+  
+          const url = 'https://hooks.zapier.com/hooks/catch/1041785/21ewe5/'
 
-					const http = new XMLHttpRequest();
-					http.open('POST', url)
-					http.onreadystatechange = function() {
-						if(http.readyState === 4 && http.status === 200) {
-							localStorage.setItem('userDetails', JSON.stringify({}))
-							navigate('thank-you')
-						}
-					}
-					http.send(JSON.stringify(formSubmitData));
-			}))
+          const http = new XMLHttpRequest();
+          http.open('POST', url)
+          http.onreadystatechange = function() {
+            if(http.readyState === 4 && http.status === 200) {
+              localStorage.setItem('userDetails', JSON.stringify({}))
+              navigate('thank-you')
+            }
+          }
+          http.send(JSON.stringify(formSubmitData));
+      }))
 
-			const isObjectEmpty = (obj = {}) => {
-				return Object.keys(obj).length === 0 && obj.constructor === Object
-			}
-	
-			const distinct = (value, index, self) => {
-				return self.indexOf(value) === index;
-			}
+      const isObjectEmpty = (obj = {}) => {
+        return Object.keys(obj).length === 0 && obj.constructor === Object
+      }
+  
+      const distinct = (value, index, self) => {
+        return self.indexOf(value) === index;
+      }
 
-			const updateUserDetailsWithCurrentUrl = (userDetails, currentUrl) => {
-				userDetails.browsedLinks = [...userDetails.browsedLinks, currentUrl]
-				userDetails.browsedLinks = userDetails.browsedLinks.filter(distinct)
-				localStorage.setItem('userDetails', JSON.stringify(userDetails))
-			}
+      const updateUserDetailsWithCurrentUrl = (userDetails, currentUrl) => {
+        userDetails.browsedLinks = [...userDetails.browsedLinks, currentUrl]
+        userDetails.browsedLinks = userDetails.browsedLinks.filter(distinct)
+        localStorage.setItem('userDetails', JSON.stringify(userDetails))
+      }
 
-			userDetails = JSON.parse(localStorage.getItem('userDetails')) || {};
-			// User already entered site atleast once
-			if (!isObjectEmpty(userDetails)) {
-				// Extract userDetails from localStorage
-				userDetails = JSON.parse(localStorage.getItem('userDetails'));
-				updateUserDetailsWithCurrentUrl(userDetails, document.URL);
-			} else {
-				// If entering first time
-				// Set referrer
-				userDetails.referrer = (!document.referrer.includes(SITE_NAME) && document.referrer) || '';
-				// Clear past links or set it empty
-				userDetails.browsedLinks = []
-				const currentTimeZone = `${parseInt(new Date().getTimezoneOffset() / -60)}:${Math.abs(new Date().getTimezoneOffset() % 60)}`
-				userDetails.timeZone = `UTC${currentTimeZone.startsWith('-') ? '' : '+'}${currentTimeZone}`
-				updateUserDetailsWithCurrentUrl(userDetails, document.URL)
-			}
-		}
-	}
-	
-	render() {
-		const { siteTitle } = this.props;
-		return (
-			<header>
-				<div>
-					<Nav 
-						logo={Logo({title: siteTitle})}
-						links={links}
-						GatsbyLink={Link}
-					/>
-				</div>
-			</header>
-		)	
-	}
+      userDetails = JSON.parse(localStorage.getItem('userDetails')) || {};
+      // User already entered site atleast once
+      if (!isObjectEmpty(userDetails)) {
+        // Extract userDetails from localStorage
+        userDetails = JSON.parse(localStorage.getItem('userDetails'));
+        updateUserDetailsWithCurrentUrl(userDetails, document.URL);
+      } else {
+        // If entering first time
+        // Set referrer
+        userDetails.referrer = (!document.referrer.includes(SITE_NAME) && document.referrer) || '';
+        // Clear past links or set it empty
+        userDetails.browsedLinks = []
+        const currentTimeZone = `${parseInt(new Date().getTimezoneOffset() / -60)}:${Math.abs(new Date().getTimezoneOffset() % 60)}`
+        userDetails.timeZone = `UTC${currentTimeZone.startsWith('-') ? '' : '+'}${currentTimeZone}`
+        updateUserDetailsWithCurrentUrl(userDetails, document.URL)
+      }
+    }
+  }
+  
+  render() {
+    const { siteTitle } = this.props;
+    return (
+      <header>
+        <div>
+          <Nav 
+            logo={Logo({title: siteTitle})}
+            links={links}
+            GatsbyLink={Link}
+          />
+        </div>
+      </header>
+    )	
+  }
 }
 
 
