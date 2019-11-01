4cf1ebf4fb1dbb710290593a0b2f7bee58d29b05
sachinmjadhav <sachin0321@gmail.com>
2019-11-02 00:25:52 +0530

    added SEO to thank-you page

--- src/pages/thank-you.js
+++ src/pages/thank-you.js
@@ -2,23 +2,32 @@ import React from "react"
 import { Link } from "gatsby"
 import { Box, P } from "bricks"
 import { Main } from "theme-ui"
+import SEO from "../components/seo"
 
 const ThankYou = () => {
   return (
-    <Main mt="7">
-      <Box textAlign="center">
-        <P fontSize="6" mb="4">
-          Thank You!
-        </P>
-        <P fontSize="3" mb="4">
-          We will get back to you within 24 hours. Our typical response time is
-          5 minutes.
-        </P>
-        <P fontSize="3">
-          <Link to="/">Back to Homepage</Link>
-        </P>
-      </Box>
-    </Main>
+    <>
+      <SEO
+        title="Thank You"
+        description="Thank you for contacting us."
+        keywords={[`ruby on rails`, `Ruby on Rails development`, `angularjs framework`, `angularjs code`, `meteor js`, `react js`, `native`, `node js development`, `node js website`, `spree commerce`, `ruby on rails developer`, `react js components`, `javascript meteor`, `Angular javascript`, `angular framework`, `js angular`, `angular website`, `Android Website`, `ios app development`, `mobile web app`, `javascript android app`, `web application development company`]}
+        url="https://codebrahma.com/thank-you"
+      />
+      <Main mt="7">
+        <Box textAlign="center">
+          <P fontSize="6" mb="4">
+            Thank You!
+          </P>
+          <P fontSize="3" mb="4">
+            We will get back to you within 24 hours. Our typical response time
+            is 5 minutes.
+          </P>
+          <P fontSize="3">
+            <Link to="/">Back to Homepage</Link>
+          </P>
+        </Box>
+      </Main>
+    </>
   )
 }
 
