17e44a29e7fda0f559b128af302ce63b869e3da7
Sachin Jadhav <34229084+sachinmjadhav@users.noreply.github.com>
2019-11-05 04:44:44 +0530

    Added Google Analytics  (#46)
    * added google-analytics and custom thank-you page
    
    * added SEO to thank-you page
    
    * remove target field on form submission
    
    * removed gatsby-plugin-google-gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,31 +39,11 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/logos/cb.png`, // This path is relative to the root of the site.
       },
-    },
-    {
-      resolve: `gatsby-plugin-google-gtag`,
+    },{
+      resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          "UA-41862404-6", // Google Analytics / GA
-        ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          optimize_id: "UA-41862404-6",
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: false,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          exclude: ["/preview/**", "/do-not-track/me/too/"],
-        },
-      },
+        trackingId: "UA-41862404-6"
+      }
     },
     {
       resolve: `gatsby-mdx`,
