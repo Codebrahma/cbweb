877e3af4f8cdfa780b08e483850a2f91e1c8a203
Kameshwaran Sachithanantham <kamesh@codebrahma.com>
2020-01-09 13:10:40 +0530

    Added a prop to plain link to optionally decorate it with underline

--- src/components/link.js
+++ src/components/link.js
@@ -1,18 +1,21 @@
-import { Link } from 'gatsby';
-import styled from '@emotion/styled'
-
+import { Link } from "gatsby"
+import styled from "@emotion/styled"
 
 const link = styled(Link, {
-  shouldForwardProp: prop =>
-    prop !== 'as'
-  })`
-    text-decoration:none;
+  shouldForwardProp: prop => prop !== "as",
+})`
+  text-decoration: ${(props) => props.withUnderline ? 'underline' : 'none'};
+  color: inherit;
+  &:visited {
     color: inherit;
-    &:visited {
-      color: inherit;
-    }
-    &:hover {
-      text-decoration: underline;
-    }
+  }
+  &:hover {
+    text-decoration: underline;
+  }
 `
-export default link;
+
+Link.defaultProps = {
+  withUnderline: false,
+};
+
+export default link
