69c19f234f49fa28ec5beac99ce52c819de699d8
Kameshwaran Sachithanantham <kamesh@codebrahma.com>
2020-02-18 15:25:51 +0530

    Modified react development page styles

---
+++ src/components/projects.js
@@ -0,0 +1,29 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Project from "./project"
+
+export const RipplingProject = () => {
+  const { screenshot } = useStaticQuery(graphql`
+    query {
+      screenshot: file(
+        relativePath: { eq: "screenshots/rippling_screenshot.png" }
+      ) {
+        childImageSharp {
+          fluid(maxWidth: 500, quality: 100) {
+            ...GatsbyImageSharpFluid
+            presentationWidth
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Project
+      title="Rippling"
+      description="HR Automation Powerhouse developed On ReactJS. Rippling is a software tool to automate a bunch of things that companies need to do while hiring and offboarding employees. Right from contract management to hardware supplies, to running the payroll and leave management – Rippling does everything."
+      image={screenshot.childImageSharp.fluid}
+      link="/rippling/"
+    />
+  )
+}
