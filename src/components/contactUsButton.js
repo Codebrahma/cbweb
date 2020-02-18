b69171a044a4ae8ddf4f7357bd7aaf90d9ba44e6
Kameshwaran Sachithanantham <kamesh@codebrahma.com>
2020-02-18 15:23:36 +0530

    Added contact us button

---
+++ src/components/contactUsButton.js
@@ -0,0 +1,21 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { navigate } from "gatsby"
+import { InputButton } from "bricks"
+
+const ContactUsButton = ({ text }) => (
+  <InputButton
+    value={text}
+    onClick={() => navigate("/contact")}  
+  />
+);
+
+ContactUsButton.defaultProps = {
+  text: 'Contact Us'
+};
+
+ContactUsButton.propTypes = {
+  text: PropTypes.string
+};
+
+export default ContactUsButton;
