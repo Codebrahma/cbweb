d40f0c1ed3d179a24441725bf5b8c2d951c33eb1
asaran97 <asaran234@gmail.com>
2020-04-30 15:44:20 +0530

    contact from validation

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -1,7 +1,17 @@
 import React, { useState } from "react";
 import ReCAPTCHA from "react-google-recaptcha";
-import { Flex, Box, InputText, OutlinedButton } from "bricks";
+import { Flex, Box, InputText, OutlinedButton, P } from "bricks";
 import { navigate } from "gatsby";
+import styled from "@emotion/styled";
+import { css } from 'bricks';
+
+const Error = styled('p')(
+  css({
+    color: 'red',
+    fontSize: 0,
+    margin: '2px 0',
+  })
+)
 
 const ContactForm = ({ referrer }) => {
   const [isCaptchaVerified, setCaptchaVerified] = useState(false);
@@ -9,11 +19,24 @@ const ContactForm = ({ referrer }) => {
     email: '',
     msg: '',
   })
+  const [errors, setErrors] = useState({
+    msg: false,
+    email: false,
+    captcha: false,
+  })
 
   const onSubmit = (e) => {
     e.preventDefault();
-    
-    if(isCaptchaVerified) {
+
+    if (!isCaptchaVerified) {
+      setErrors({ ...errors, captcha: true})
+    } else {
+      setErrors({
+        msg: false,
+        email: false,
+        captcha: false,
+      });
+
       let xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
@@ -26,7 +49,22 @@ const ContactForm = ({ referrer }) => {
     }
   }
 
-  const onChange = ({target: { name, value }}) => setFormvalues({ ...formValues, [name]: value})
+  const onChange = ({target: { name, value }}) => {
+    if (name === 'msg') {
+      if (new RegExp(/[^\u0000-\u007F]+/).test(value)) {
+        setErrors({ ...errors, msg: true})
+      } else {
+        setErrors({ ...errors, msg: false})
+      }
+    } else if (name === 'email') {
+      if (new RegExp(/\.ru$/).test(value)) {
+        setErrors({ ...errors, email: true})
+      } else {
+        setErrors({ ...errors, email: false})
+      }
+    }
+    setFormvalues({ ...formValues, [name]: value})
+  }
 
   return (
     <form onSubmit={onSubmit}>
@@ -52,6 +90,7 @@ const ContactForm = ({ referrer }) => {
             value={formValues.msg}
             onChange={onChange}
           />
+          {errors.msg && <Error>Please enter the message in English</Error>}
         </Box>
         <Box width={[1, 1 / 2]} mt={1}>
           <InputText
@@ -66,12 +105,14 @@ const ContactForm = ({ referrer }) => {
             value={formValues.email}
             onChange={onChange}
           />
+          {errors.email && <Error>Please provide a valid email</Error>}
         </Box>
         <Box mt='1'>
           <ReCAPTCHA
             sitekey="6LeFW-8UAAAAABYFkKUu6fKNwYFL-p6JngDJV4jI"
             onChange={() => setCaptchaVerified(!isCaptchaVerified)}
           />
+          {!isCaptchaVerified && errors.captcha && <Error>Please verify you are not a robot</Error>}
         </Box>
         <Box width={[1, 1 / 3]} mt={1}>
           <OutlinedButton borderRadius={3}>
