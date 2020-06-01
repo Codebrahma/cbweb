f908c5ddee780cc54a0321ba792a76910c1ef9a6
asaran97 <asaran234@gmail.com>
2020-06-02 01:17:07 +0530

    changed theme

--- src/theme.js
+++ src/theme.js
@@ -1,16 +1,24 @@
+import { theme } from "@chakra-ui/core"
+
 const maxContainerWidth = '70rem'
 const baseLineHeight = 1.45; //24.8px
 const desktopBaseSize = 1.125; //18px
 const mobileBaseSize = 1; //16px
 const breakpoints = [
-  '52em',
+  '48em',
 ]
 const scale = [
   0.75,1, 1.2, 1.375, 1.6875, 2.5
 ];
 
 const fontSizes = scale.map(n=> n*mobileBaseSize + 'rem')
-fontSizes.desktop = scale.map(n=> n*desktopBaseSize + 'rem')
+//desktops
+fontSizes['xs'] = scale[0]*2 + 'rem'
+fontSizes['sm'] = scale[1]*2 + 'rem'
+fontSizes['md'] = scale[2]*2 + 'rem'
+fontSizes['lg'] = scale[3]*2 + 'rem'
+fontSizes['xl'] = scale[4]*2 + 'rem'
+fontSizes['2xl'] = scale[5]*2 + 'rem'
 
 const fonts = {
   heading: 'TiemposHeadline',
@@ -18,7 +26,9 @@ const fonts = {
   bold: 'TiemposBold',
   italic: 'TiemposItalic'
 }
+
 const radii = [0, 1, 2, 4, 8, 16];
+
 const lineHeights = [
   0.5 * baseLineHeight + 'rem',
   1 * baseLineHeight + 'rem', 
@@ -27,6 +37,7 @@ const lineHeights = [
   2.5 * baseLineHeight + 'rem',
   3 * baseLineHeight + 'rem',
 ];
+
 const space = [0,...lineHeights];
 
 const colors = {
@@ -36,101 +47,102 @@ const colors = {
 colors.primary = colors.black[1]
 colors.secondary = colors.tint
 
-const styles = {
-  body: {
-    fontFamily: 'body',
-    fontSize: [1, 'desktop.1'],
-    lineHeight: 1,
-  },
-  h1: {
-    fontFamily: 'heading',
-    fontSize: [5, 'desktop.5'],
-    lineHeight: 3,
-    marginTop: [3, 4], 
-    marginBottom: 1,
-    padding: 0,
-  },
-  h2: {
-    fontFamily: 'heading',
-    fontSize: [4, 'desktop.4'],
-    lineHeight: 2,
-    marginTop: [3, 4],
-    marginBottom: 1,
-    padding: 0,
-  },
-  h3: {
-    fontFamily: 'heading',
-    fontSize: [3, 'desktop.3'],
-    lineHeight: 1,
-    marginTop: [ 2,3 ],
-    marginBottom: 1,
-    padding: 0,
-  },
-  h4: {
-    fontFamily: 'heading',
-    fontSize: [2, 'desktop.2'],
-    lineHeight: 1,
-    marginTop: 2,
-    marginBottom: 1,
-    padding: 0,
-  },
-  h5: {
-    fontFamily: 'heading',
-    fontSize: [1, 'desktop.1'],
-    lineHeight: 1,
-    marginTop: 2,
-    marginBottom: 1,
-    padding: 0,
-  },
-  p: {
-    fontFamily: 'body',
-    fontSize: [1, 'desktop.1'],
-    lineHeight: 1,
-    marginBottom: [2,2]
-  },
-  b: {
-    fontFamily: 'bold',
-  },
-  strong: {
-    fontFamily: 'bold'
-  },
-  em: {
-    fontFamily: 'italic'
-  },
-  i: {
-    fontFamily: 'italic',
-  },
-  Container: {
-    maxWidth: '71.5rem',
-    padding: 0,
-    px: ['0.725rem']
-  },
-  ul: {
-    listStyle: 'disc inside none' ,
-    mb: [2,2]
-  },
-  ol: {
-    listStyle: 'decimal inside none',
-  },
-  li: {
-    mb: ['0.5rem','0.5rem'],
-    fontFamily: 'body',
-    fontSize: [1, 'desktop.1'],
-    lineHeight: 1,
-    maxWidth: '40rem',
-    marginBottom: [1,1]
-  }
-}
+// const styles = {
+//   body: {
+//     fontFamily: 'body',
+//     fontSize: [1, 'desktop.1'],
+//     lineHeight: 1,
+//   },
+//   h1: {
+//     fontFamily: 'heading',
+//     fontSize: [5, 'desktop.5'],
+//     lineHeight: 3,
+//     marginTop: [3, 4], 
+//     marginBottom: 1,
+//     padding: 0,
+//   },
+//   h2: {
+//     fontFamily: 'heading',
+//     fontSize: [4, 'desktop.4'],
+//     lineHeight: 2,
+//     marginTop: [3, 4],
+//     marginBottom: 1,
+//     padding: 0,
+//   },
+//   h3: {
+//     fontFamily: 'heading',
+//     fontSize: [3, 'desktop.3'],
+//     lineHeight: 1,
+//     marginTop: [ 2,3 ],
+//     marginBottom: 1,
+//     padding: 0,
+//   },
+//   h4: {
+//     fontFamily: 'heading',
+//     fontSize: [2, 'desktop.2'],
+//     lineHeight: 1,
+//     marginTop: 2,
+//     marginBottom: 1,
+//     padding: 0,
+//   },
+//   h5: {
+//     fontFamily: 'heading',
+//     fontSize: [1, 'desktop.1'],
+//     lineHeight: 1,
+//     marginTop: 2,
+//     marginBottom: 1,
+//     padding: 0,
+//   },
+//   p: {
+//     fontFamily: 'body',
+//     fontSize: [1, 'desktop.1'],
+//     lineHeight: 1,
+//     marginBottom: [2,2]
+//   },
+//   b: {
+//     fontFamily: 'bold',
+//   },
+//   strong: {
+//     fontFamily: 'bold'
+//   },
+//   em: {
+//     fontFamily: 'italic'
+//   },
+//   i: {
+//     fontFamily: 'italic',
+//   },
+//   Container: {
+//     maxWidth: '71.5rem',
+//     padding: 0,
+//     px: ['0.725rem']
+//   },
+//   ul: {
+//     listStyle: 'disc inside none' ,
+//     mb: [2,2]
+//   },
+//   ol: {
+//     listStyle: 'decimal inside none',
+//   },
+//   li: {
+//     mb: ['0.5rem','0.5rem'],
+//     fontFamily: 'body',
+//     fontSize: [1, 'desktop.1'],
+//     lineHeight: 1,
+//     maxWidth: '40rem',
+//     marginBottom: [1,1]
+//   }
+// }
 
-const theme = {
+const customTheme = {
+  ...theme,
   breakpoints,
   space,
   fontSizes,
   radii,
-  lineHeights,
-  maxContainerWidth,
+  // lineHeights,
+  // maxContainerWidth,
   fonts,
   colors,
-  styles,
+  // styles,
 }
-export default theme;
+export default customTheme;
