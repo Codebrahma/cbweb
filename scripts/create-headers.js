e94a830286c0c5b5a9a35f6bc0a1e075806bf2c7
Kameshwaran Sachithanantham <kamesh@codebrahma.com>
2021-05-29 21:45:58 +0530

    Create expiry header

---
+++ scripts/create-headers.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+fs.writeFileSync('./public/_headers', `/*.svg
+  Cache-Control: public, max-age=31536000
+/*.png
+  Cache-Control: public, max-age=31536000
+/*.ttf
+  Cache-Control: public, max-age=31536000
+/favicon.ico
+  Cache-Control: public, max-age=31536000
+`);
