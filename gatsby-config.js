635bb45977221c0c8b4d8475fb3c4e43edf60037
Kameshwaran Sachithanantham <kamesh@codebrahma.com>
2021-05-31 00:32:02 +0530

    Enable gZip

--- gatsby-config.js
+++ gatsby-config.js
@@ -122,7 +122,12 @@ module.exports = {
         enableImprovedAccessibility: false // Optional. Sets aria-label attribute on pop-up icon for screen readers. Defaults to true.
       },
     },
-    'gatsby-plugin-zopfli',
+    {
+      resolve: 'gatsby-plugin-zopfli',
+      options: {
+        extensions: ['css', 'html', 'js', 'png']
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
